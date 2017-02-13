1a5db2de386cca52afbdbb6ba82c77ec78cf92f7
Luciano Reis <lucianoreis.dev@gmail.com>
2017-02-13 16:01:13 -0300

    [UPD] Botoes Datatable

--- resources/views/unidade-medida/index.blade.php
+++ resources/views/unidade-medida/index.blade.php
@@ -1,59 +1,18 @@
 @extends('layouts.default')
 @section('content')
-
-        <div class="card-box table-responsive">
-            <table id="datatable-listagem" class="display table table-hover table-bordered table-striped " cellspacing="0" width="100%">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Unidade Medida</th>
-                        <th>Sigla</th>
-                    </tr>
-                </thead>
-                <tfoot>
-                </tfoot>
-            </table>    
-          <hr>
-          <div id="teste">TESTE</div>
-        </div>
-
-<?php echo $model->appends(Request::session()->get('unidade-medida.index'))->render();?>
-
-
-
+<div class="card-box table-responsive">
+    <table id="datatable-listagem" class="display table table-hover table-bordered table-striped " cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Unidade Medida</th>
+                <th>Sigla</th>
+            </tr>
+        </thead>
+    </table>
+    <div class="botoes"></div>
+</div>
 @section('inscript')
-
-<script type="text/javascript">
-    $(document).ready(function () {
-        
-        var table = $('#datatable-listagem').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: '{{ url('unidade-medida/datatable-listagem') }}',
-            lengthChange: false,
-            /*
-            */
-            buttons: ['copy', 'excel', 'pdf', 'colvis']
-        });
-        /*
-        table.buttons().container()
-            .appendTo('#teste');
-        */
-        /*
-        //Buttons examples
-        var table = $('#datatable-buttons').DataTable({
-            lengthChange: false,
-            buttons: ['copy', 'excel', 'pdf', 'colvis']
-        });
-
-        table.buttons().container()
-            .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
-            */
-    });
-
-</script>
-<script type="text/javascript"></script>
-
 <link href="{{ URL::asset('public/assets/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css"/>
 <link href="{{ URL::asset('public/assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css"/>
 <link href="{{ URL::asset('public/assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css"/>
@@ -75,24 +34,26 @@
 <!-- Responsive examples -->
 <script src="{{ URL::asset('public/assets/plugins/datatables/dataTables.responsive.min.js') }}"></script>
 <script src="{{ URL::asset('public/assets/plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        
+        var table = $('#datatable-listagem').DataTable({
+            dom: 'Bfrtip',
+            language: {
+                url: "{{ URL::asset('public/assets/plugins/datatables/Portuguese-Brasil.lang') }}"
+            },
+            processing: true,
+            serverSide: true,
+            ajax: '{{ url('unidade-medida/datatable-listagem') }}',
+            lengthChange: false,
+            buttons: ['copy', 'excel', 'pdf', 'print']
+        });
+        //table.buttons().container()
+            //.appendTo('.botoes');        
+            //.appendTo('#datatable-buttons_wrapper .row .col-md-6:eq(0)');        
 
+    });
 
-
+</script>
 @endsection
-
-
-
-<p>
-    <a href="{{ url("unidade-medida/create") }}">Nova</a>
-</p>
-<ul>
-    @foreach($model as $row)
-    <li>
-        <a href="{{ url("unidade-medida/$row->codunidademedida") }}">
-            {{ formataCodigo($row->codunidademedida) }} | {{ $row->unidademedida }} | {{ $row->sigla }}
-        </a>
-        <hr>
-    </li>
-    @endforeach
-</ul>
 @stop
