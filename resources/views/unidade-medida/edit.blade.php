a0c1135328f63fd4468bd22842a5d3385c3aec24
Fabio Migliorini <fabio@mgpapelaria.com.br>
2017-02-13 10:12:43 -0300

    [UPD] Layout Manutenção

--- resources/views/unidade-medida/edit.blade.php
+++ resources/views/unidade-medida/edit.blade.php
@@ -1,13 +1,10 @@
 @extends('layouts.default')
 @section('content')
-<h1>Unidade de Medida | Nova</h1>
-<p>
-    <a href="{{ url("unidade-medida") }}">Listagem</a>
-</p>
-
-{!! Form::model($model, ['method' => 'PATCH', 'class' => 'form-horizontal', 'id' => 'form-unidade-medida', 'action' => ['UnidadeMedidaController@update', $model->codunidademedida] ]) !!}
+<div class="card-box col-md-6">
+{!! Form::model($model, ['method' => 'PATCH', 'id' => 'form-unidade-medida', 'action' => ['UnidadeMedidaController@update', $model->codunidademedida] ]) !!}
     @include('unidade-medida.form', ['submitTextButton' => 'Salvar'])
 {!! Form::close() !!}
+</div>
 @section('inscript')
 <script type="text/javascript"></script>
 @endsection
--- resources/views/unidade-medida/show.blade.php
+++ resources/views/unidade-medida/show.blade.php
@@ -1,31 +1,38 @@
 @extends('layouts.default')
 @section('content')
-<h1>Unidade de Medida | {{ $model->unidademedida }}</h1>
-<p>
-    <a href="{{ url("unidade-medida") }}">Listagem</a> | 
-    <a href="{{ url("unidade-medida/create") }}">Nova</a> | 
-    <a href="{{ url("unidade-medida/$model->codunidademedida/edit") }}">Editar</a>
-</p>
-<div class="row">
-  <div class="col-lg-12">
-      <table class="detail-view table table-striped table-condensed" border="1"> 
-        <tbody>  
-          <tr> 
-            <th class="col-md-2">#</th> 
-            <td class="col-md-10">{{ $model->codunidademedida }}</td> 
-          </tr>
-          <tr> 
-            <th>Descrição</th> 
-            <td>{{ $model->unidademedida }}</td> 
-          </tr>
-          <tr> 
-            <th>Sigla</th> 
-            <td>{{ $model->sigla }}</td> 
-          </tr>
-        </tbody> 
-      </table>
-  </div>    
+<div class="card-box col-md-5">
+  <h4 class="header-title m-t-0">
+    &nbsp;
+    <span class="pull-right">
+      <a href="{{ url("unidade-medida/create") }}"><i class="fa fa-plus"></i></a> 
+      <a href="{{ url("unidade-medida/$model->codunidademedida/edit") }}"><i class="fa fa-pencil"></i></a>
+    </span>
+  </h4>  
+    <table class="table table-bordered table-striped table-hover table-sm col-md-6">
+      <tbody>  
+        <tr> 
+          <th>#</th> 
+          <td>{{ $model->codunidademedida }}</td> 
+        </tr>
+        <tr> 
+          <th>Descrição</th> 
+          <td>{{ $model->unidademedida }}</td> 
+        </tr>
+        <tr> 
+          <th>Sigla</th> 
+          <td>{{ $model->sigla }}</td> 
+        </tr>
+      </tbody> 
+    </table>
+  
+    <small class="text-muted">
+                Criado
+            em 07/02/2017 15:54             por <a href=http://192.168.1.205/MGLara/usuario/10000004>alisson</a> 
+                    Alterado
+            em 13/02/2017 09:50             por <a href=http://192.168.1.205/MGLara/usuario/1>fabio</a> 
+    </small>  
 </div>
+
 @section('inscript')
 <script type="text/javascript"></script>
 @endsection
